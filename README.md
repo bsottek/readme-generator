f3ad4a1ef8b340876f6311098dd4922cf23a538c
bsottek <bsottek@gmail.com>
2021-07-25 21:23:20 -0500

    inquirer prompts and page generation

--- README.md
+++ README.md
@@ -1,3 +1,43 @@
-# Professional README Generator Starter Code
 
-[How to create a Professional README](./readme-guide.md)
+# Readme Generator
+## Description 
+    
+Streamline the process of readme creation by taking user prompts on the command line and generating a profesional-quality readme file.
+    
+## Table of Contents
+    
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Preview](#preview)
+* [License](#license)
+* [Contributing](#contributing)
+* [Questions](#questions)
+    
+    
+## Installation
+    
+Install both Node.js and Inquirer through npm.
+    
+## Preview 
+    
+![alt text](assets/screenshot.jpg)
+## Usage
+Generate properly-formatted, high-quality readme for your projects.
+    
+    
+## License
+    
+![badge](https://img.shields.io/badge/license-Open-brightgreen)
+    
+    
+## Contributing
+    
+Ben Sottek
+
+## Questions
+Contact me via email or github<br />
+<br />
+Find me on GitHub: [bsottek](https://github.com/bsottek)<br />
+<br />
+Email me with any questions: bsottek@gmail.com<br /><br />
--- index.js
+++ index.js
@@ -1,13 +1,124 @@
-// TODO: Include packages needed for this application
+// Declaring the dependencies and variables
+const fs = require("fs");
+const inquirer = require("inquirer");
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// generateReadme function populating the README.md
+function generateReadme(answers) {
+    return `
+# ${answers.projectTitle}
+## Description 
+    
+${answers.description}
+    
+## Table of Contents
+    
+* [Description](#description)
+* [Installation](#installation)
+* [Usage](#usage)
+* [Preview](#preview)
+* [License](#license)
+* [Contributing](#contributing)
+* [Questions](#questions)
+    
+    
+## Installation
+    
+${answers.installation}
+    
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+## Usage
+${answers.usage}
+    
+## Preview
 
-// TODO: Create a function to initialize app
-function init() {}
+![alt text](assets/screenshot.jpg)
+    
+## License
+    
+![badge](https://img.shields.io/badge/license-${answers.license}-brightgreen)
+    
+    
+## Contributing
+    
+${answers.contributing}
 
-// Function call to initialize app
-init();
+## Questions
+${answers.questions}<br />
+<br />
+Find me on GitHub: [${answers.username}](https://github.com/${answers.username})<br />
+<br />
+Email me with any questions: ${answers.email}<br /><br />`
+};
+
+return inquirer.prompt([
+    {
+        type: "input",
+        name: "projectTitle",
+        message: "What is the project title?",
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Write a brief description of your project: "
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Describe the installation process if any: ",
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "What is this project used for?"
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "Chose the appropriate license for this project: ",
+        choices: [
+            "Apache",
+            "Academic",
+            "GNU",
+            "ISC",
+            "MIT",
+            "Mozilla",
+            "Open"
+        ]
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Who are the contributors of this projects?"
+    },
+    {
+        type: "input",
+        name: "tests",
+        message: "Is there a test included?"
+    },
+    {
+        type: "input",
+        name: "questions",
+        message: "What do I do if I have an issue? "
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "Please enter your GitHub username: "
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "Please enter your email: "
+    }
+])
+    .then(function (answer) {
+
+        fs.open('README.md', 'w', function (err, file) {
+            if (err) throw err;
+            console.log('Created!');
+        });
+
+        fs.appendFile('README.md', generateReadme(answer), function (err) {
+            if (err) throw err;
+        });
+    });
